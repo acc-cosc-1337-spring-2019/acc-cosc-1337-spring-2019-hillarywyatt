9afc81e22baaa1f2e254185878ce320d2073566f
hillarywyatt <hillary.wyatt@g.austincc.edu>
2019-01-31 19:17:42 -0600

    1/31 Coniditonal Structure examples/ test cases

--- examples/01_module/06_if_else/if_else.h
+++ examples/01_module/06_if_else/if_else.h
@@ -1,4 +1,8 @@
 //write prototype for function named get_generation that accepts an int and
 //returns a string
+#include <string>
 
+using std::string;
+
+string get_generation(int year);
 
--- examples/01_module_test/01_module_test.cpp
+++ examples/01_module_test/01_module_test.cpp
@@ -1,14 +1,28 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "output.h"
+#include "if.h"
+#include"if_else.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
 }
 
 //Create a test case for the if statement using:
 //the number 4 result should be true
 //the number 55 result should be false
+TEST_CASE("Test is even")
+{
+	REQUIRE(is_even(4) == true);
+	REQUIRE(is_even(55) == false);
+}
+
+TEST_CASE("Test overtime function")
+{
+	REQUIRE(overtime(41) == true);
+	REQUIRE(overtime(35) == false);
+}
 
 //Create a test case for the if else statement using:
 //the year 1930 result should be 'Silent Generation'
@@ -18,7 +32,16 @@ TEST_CASE("Verify Test Configuration", "verification") {
 //the year 1996 result should be 'Centennial'
 //the year 1900 result should be 'Invalid Year'
 //the year 2019 result should be 'Invalid Year'
-
+TEST_CASE("Test get-generation function")
+{
+	REQUIRE(get_generation(1930) == "Silent Genertaion");
+	REQUIRE(get_generation(1946) == "Baby Boomer");
+	REQUIRE(get_generation(1970) == "Genertaion X");
+	REQUIRE(get_generation(1995) == "Millenial");
+	REQUIRE(get_generation(1996) == "Centenial");
+	REQUIRE(get_generation(1900) == "Invalid Year");
+	REQUIRE(get_generation(2019) == "Invalid Year");
+}
 
 //Create a test case for the switch statement using:
 //value 1 result should be 'Option 1'
@@ -26,4 +49,3 @@ TEST_CASE("Verify Test Configuration", "verification") {
 //value 3 result should be 'Option 3'
 //value 4 result should be 'Option 4'
 //value 5 result should be 'Invalid Option'
-
