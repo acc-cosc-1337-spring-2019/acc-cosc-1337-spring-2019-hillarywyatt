8927cba88cb769710946377b5058f6fff1a7e7e3
hillarywyatt <hillary.wyatt@g.austincc.edu>
2019-02-07 17:50:00 -0600

    2/5 classwork

--- examples/01_module/07_switch/switch.h
+++ examples/01_module/07_switch/switch.h
@@ -1,3 +1,5 @@
 //Write prototype for function that accepts a num and returns a string
 
+#include<string>
 
+std::string menu_option(int num);
--- examples/01_module_test/01_module_test.cpp
+++ examples/01_module_test/01_module_test.cpp
@@ -3,6 +3,7 @@
 #include "output.h"
 #include "if.h"
 #include"if_else.h"
+#include"switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") 
 {
@@ -49,3 +50,11 @@ TEST_CASE("Test get-generation function")
 //value 3 result should be 'Option 3'
 //value 4 result should be 'Option 4'
 //value 5 result should be 'Invalid Option'
+TEST_CASE("Test menu option function")
+{
+	REQUIRE(menu_option(1) == "Option 1");
+	REQUIRE(menu_option(2) == "Option 2");
+	REQUIRE(menu_option(3) == "Option 3");
+	REQUIRE(menu_option(4) == "Option 4");
+	REQUIRE(menu_option(100) == "Invalid Option");
+}
