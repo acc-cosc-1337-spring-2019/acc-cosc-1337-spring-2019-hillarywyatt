0af302b460d35e356035a9f6b5b39fbd2db72b24
hillarywyatt <wyatt.hillary@gmail.com>
2019-02-07 20:20:49 -0600

    2/7 assignment

--- classwork/03_assign/main.cpp
+++ classwork/03_assign/main.cpp
@@ -1,4 +1,8 @@
+#include "loops.h"
+#include<iostream>
 
+using::std::cin;
+using::std::cout;
 /*
 Use a do while loop to prompt the user for 
 a number, call the factorial function, and display the number's
@@ -6,5 +10,17 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
-	return 0;
+	int num;
+	char choice;
+	do 
+	{
+		cout << "Enter a number: ";
+		cin >> num;
+
+		cout << factorial_of_num(num);
+		cout << "Do you want to continue: Y or N";
+		cin >> choice;
+
+	} while (choice == 'y' || choice == 'Y');
+	return choice;
 }
--- classwork/03_assign_tests/loops_tests.cpp
+++ classwork/03_assign_tests/loops_tests.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "loops.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -11,3 +12,9 @@ factorial of 3 returns   6
 factorial of 4 returns  24
 factorial if 5 returns 120
 */
+TEST_CASE("Test factorial fucntion")
+{
+	REQUIRE(factorial_of_num(3) == 6);
+	REQUIRE(factorial_of_num(4) == 24);
+	REQUIRE(factorial_of_num(5) == 120);
+}
